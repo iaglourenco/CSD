ca644c429461296950573e2c0db4a7a300425d96
Iago <iagojlourenco@gmail.com>
2022-10-29 14:54:43 -0300

    Atualizado docs, maquina iniciada

---
+++ maquina-virtual/js/maquina.js
@@ -0,0 +1,73 @@
+// Author: Iago Lourenço (iagojlourenco@gmail.com) / maquina.js
+var filename = "Novo.lpdo";
+
+async function loadLocalFile(files) {
+  var reader = new FileReader();
+  reader.fileName = files[0].name;
+  filename = files[0].name;
+
+  reader.onload = function (e) {
+    logar(`Arquivo ${e.target.fileName} carregado com sucesso!`);
+    // editor.setValue(e.target.result);
+  };
+  reader.readAsText(files[0]);
+}
+
+// Exibe mensagens no log com a hora
+function logar(msg) {
+  document.getElementById(
+    "log"
+  ).value += `[${new Date().toLocaleString()}] ${msg}\n`;
+  // Faz o scroll na textarea para o fim
+  document.getElementById("log").scrollTop =
+    document.getElementById("log").scrollHeight;
+}
+
+window.onload = function () {
+  logar("Bem vindo à máquina virtual LPD!");
+  logar(
+    "Para começar, carregue um arquivo .lpdo clicando no botão 'Carregar' acima."
+  );
+
+  // Animação do logo
+  document.getElementById("logo").addEventListener("mouseover", function () {
+    document.getElementsByClassName("logo_snip")[0].innerHTML = "return";
+  });
+
+  // Animação do logo
+  document.getElementById("logo").addEventListener("mouseout", function () {
+    document.getElementsByClassName("logo_snip")[0].innerHTML = "LPD";
+  });
+
+  // Intercepta pressionamento de teclas na página
+  document.onkeydown = function (e) {
+    // Ctrl + F5
+    if (e.ctrlKey && e.keyCode == 116) {
+      e.preventDefault();
+      document.getElementById("debug").click();
+    }
+    // F5
+    if (e.keyCode == 116) {
+      e.preventDefault();
+      document.getElementById("executar").click();
+    }
+    // Ctrl + L
+    if (e.ctrlKey && e.keyCode == 76) {
+      e.preventDefault();
+      document.getElementById("limpar").click();
+    }
+    // Ctrl + O
+    if (e.ctrlKey && e.keyCode == 79) {
+      e.preventDefault();
+      document.getElementById("abrir").click();
+    }
+  };
+
+  // Carrega o arquivo local
+  document.getElementById("abrir").addEventListener("click", function () {
+    document.getElementById("file-input").click();
+  });
+  document.getElementById("file-input").addEventListener("change", function () {
+    loadLocalFile(this.files);
+  });
+};
