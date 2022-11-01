0c9f2a8bc552d86c2a0a4cca6d5bd774cfe3888d
Iago <iagojlourenco@gmail.com>
2022-11-01 13:01:44 -0300

    Layout e funções básicas da VM feitas

--- maquina-virtual/js/maquina.js
+++ maquina-virtual/js/maquina.js
@@ -1,5 +1,6 @@
 // Author: Iago Lourenço (iagojlourenco@gmail.com) / maquina.js
 var filename = "Novo.lpdo";
+var code = "";
 
 async function loadLocalFile(files) {
   var reader = new FileReader();
@@ -8,7 +9,10 @@ async function loadLocalFile(files) {
 
   reader.onload = function (e) {
     logar(`Arquivo ${e.target.fileName} carregado com sucesso!`);
-    // editor.setValue(e.target.result);
+    code = e.target.result;
+    load2Table(code);
+    document.getElementById("executar").style.transform = "translateX(0px)";
+    document.getElementById("debug").style.transform = "translateX(0px)";
   };
   reader.readAsText(files[0]);
 }
@@ -21,6 +25,14 @@ function logar(msg) {
   // Faz o scroll na textarea para o fim
   document.getElementById("log").scrollTop =
     document.getElementById("log").scrollHeight;
+}
+
+function input(message) {
+  document.getElementById("input").value += `${message}\n`;
+}
+
+function output(message) {
+  document.getElementById("output").value += `${message}\n`;
 }
 
 window.onload = function () {
@@ -83,6 +95,11 @@ window.onload = function () {
     document.getElementById("drag_and_drop").classList.remove("hovered");
   });
 
+  document.getElementById("fechar").addEventListener("click", closeDoc);
+
+  window.addEventListener("resize", setupPrank);
+  setupPrank();
+
   // Carrega o arquivo local
   document.getElementById("abrir").addEventListener("click", function () {
     document.getElementById("file-input").click();
@@ -91,3 +108,102 @@ window.onload = function () {
     loadLocalFile(this.files);
   });
 };
+
+function prank() {
+  let executar = document.getElementById("executar");
+  let debug = document.getElementById("debug");
+  if (code.length > 0) {
+    executar.style.transform = "translateX(0px)";
+    debug.style.transform = "translateX(0px)";
+  }
+  // Both right
+  else if (
+    executar.style.transform == "translateX(0px)" &&
+    debug.style.transform == "translateX(0px)"
+  ) {
+    if (this.id == "executar") {
+      executar.style.transform = "translateX(-100px)";
+    } else {
+      executar.style.transform = "translateX(-100px)";
+      debug.style.transform = "translateX(-100px)";
+    }
+  }
+  // Both left
+  else if (
+    executar.style.transform == "translateX(-100px)" &&
+    debug.style.transform == "translateX(-100px)"
+  ) {
+    if (this.id == "executar") {
+      executar.style.transform = "translateX(0px)";
+      debug.style.transform = "translateX(0px)";
+    } else {
+      debug.style.transform = "translateX(0px)";
+    }
+  }
+  // executar right and debug left
+  else if (
+    executar.style.transform == "translateX(-100px)" &&
+    debug.style.transform == "translateX(0px)"
+  ) {
+    if (this.id == "executar") {
+      executar.style.transform = "translateX(0px)";
+    } else {
+      debug.style.transform = "translateX(-100px)";
+    }
+  }
+}
+
+function load2Table(code) {
+  // Carrega o código para a tabela na coluna de instruções
+  // com a primeira coluna sendo um checkbox, a segunda o code e a terceira a descrição
+  let table = document.getElementById("code_table_body");
+  let lines = code.split("\n");
+  table.innerHTML = "";
+  for (let i = 0; i < lines.length - 1; i++) {
+    let line = lines[i];
+    let row = table.insertRow(i);
+    let cell = row.insertCell(0);
+    // zero fill left
+    cell.innerHTML = `${(i + 1)
+      .toString()
+      .padStart(2, "0")} <input type="checkbox" id="checkbox${i}">`;
+    cell = row.insertCell(1);
+    cell.innerHTML = line;
+    cell = row.insertCell(2);
+    cell.innerHTML = "";
+  }
+  document.getElementsByClassName("drag_and_drop")[0].classList.add("hidden");
+  document.getElementsByClassName("code")[0].classList.remove("hidden");
+}
+
+function setupPrank() {
+  let executar = document.getElementById("executar");
+  let debug = document.getElementById("debug");
+  executar.style.transform = "translateX(0px)";
+  debug.style.transform = "translateX(0px)";
+
+  if (window.innerWidth > 768) {
+    executar.addEventListener("mouseenter", prank);
+    debug.addEventListener("mouseenter", prank);
+  } else {
+    executar.removeEventListener("mouseenter", prank);
+    debug.removeEventListener("mouseenter", prank);
+  }
+}
+
+function closeDoc() {
+  code = "";
+  document.getElementById("code_table_body").innerHTML = "";
+  document.getElementById("input").value = "";
+  document.getElementById("output").value = "";
+  document.getElementById("log").value = "";
+  document
+    .getElementsByClassName("drag_and_drop")[0]
+    .classList.remove("hidden");
+  document.getElementsByClassName("code")[0].classList.add("hidden");
+
+  logar("Bem vindo à máquina virtual LPD!");
+  logar(
+    "Para começar, carregue um arquivo .lpdo clicando no botão 'Carregar' acima ou arrastando o arquivo."
+  );
+}
